65967de982af422f21f5e1b603eda6cb31757d4f
muirenee <67049659+muirenee@users.noreply.github.com>
2026-08-12 08:03:41 +0200

    Update in-app version for 1.0.1

--- lib/core/app_info.dart
+++ lib/core/app_info.dart
@@ -2,8 +2,8 @@ class AppInfo {
   const AppInfo._();
 
   static const String name = 'Netsource Sentinel';
-  static const String version = '1.0.0';
-  static const int buildNumber = 100;
+  static const String version = '1.0.1';
+  static const int buildNumber = 101;
   static const String packageId = 'com.netsource.sentinel';
   static const String supportEmail = 'support@fidalix.com';
 
