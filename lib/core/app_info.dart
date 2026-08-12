b480e11eb76a43a34f391f79cb2c1294ca9ed064
muirenee <67049659+muirenee@users.noreply.github.com>
2026-08-12 09:01:20 +0200

    Update app info for 1.0.2

--- lib/core/app_info.dart
+++ lib/core/app_info.dart
@@ -2,8 +2,8 @@ class AppInfo {
   const AppInfo._();
 
   static const String name = 'Netsource Sentinel';
-  static const String version = '1.0.1';
-  static const int buildNumber = 101;
+  static const String version = '1.0.2';
+  static const int buildNumber = 102;
   static const String packageId = 'com.netsource.sentinel';
   static const String supportEmail = 'support@fidalix.com';
 
